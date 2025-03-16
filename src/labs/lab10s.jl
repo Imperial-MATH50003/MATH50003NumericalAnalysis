31f77b1eaa98250e971c58ddd5790011448eb3c5
Sheehan Olver <solver@mac.com>
2025-03-16 20:12:28 +0000

    Start Lab 10

---
+++ src/labs/lab10s.jl
@@ -0,0 +1,19 @@
+# # MATH50003 (2024–25)
+# # Lab 10: VI.3 Gaussian Quadrature
+
+# **Learning Outcomes**
+#
+# Mathematical knowledge:
+
+
+# Coding knowledge:
+
+# 1. `eigen` for computing eigenvalues and eigenvectors
+
+# ## VI.3.1 Roots of orthogonal polynomials and truncated Jacobi matrices
+
+# **Problem** Compute all 100 roots of the Chebyshev U polynomial $U_{100}(x)$.
+
+# **Problem** Compute all 100 roots of the Legendre polynomial $P_100(x)$.
+
+# ## VI.3.2 Properties of Gaussian quadrature
